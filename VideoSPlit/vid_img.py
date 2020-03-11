7d7086c1f879c651df37547c578368d5c012ec2b
Divya Mohan <divyananjilath@gmail.com>
2020-03-11 17:12:04 +0000

    Upload New File

---
+++ VideoSPlit/vid_img.py
@@ -0,0 +1,17 @@
+import cv2
+vidcap = cv2.VideoCapture('sample.webm')
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("image"+str(100+count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.3 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
